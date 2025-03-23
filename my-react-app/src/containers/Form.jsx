acf35ae38dfd1194192b544020f788c2d5543d16
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-03-23 17:03:00 +0100

    Mise en place d'un systeme d'authentification simple avec placement du token dans le localstorage

--- my-react-app/src/containers/Form.jsx
+++ my-react-app/src/containers/Form.jsx
@@ -1,15 +1,7 @@
-import Button from "../components/Button";
-import Field from "../components/Field";
-import { BUTTON_TYPES } from "../helpers/buttonsTypes";
-import { FIELD_TYPES } from '../helpers/fieldTypes';
-
-function Form() {
+function Form({children, onSubmit}) {
   return (
-    <form>
-        <Field label="Username" id="username" name="username" className="input-wrapper" required/>
-        <Field type={FIELD_TYPES.INPUT_PASSWORD} label="Password" id="password" name="password" className="input-wrapper" required/>
-        <Field type={FIELD_TYPES.INPUT_CHECKBOX} label="Remember me" id="remember-me" name="password" className="input-remember" required/>
-        <Button type={BUTTON_TYPES.SUBMIT} onClick={() => null} className="sign-in-button" children="Sign In"/>
+    <form onSubmit={onSubmit}>
+      {children}
     </form>
   );
 };
--- my-react-app/src/pages/SignIn.jsx
+++ my-react-app/src/pages/SignIn.jsx
@@ -1,16 +1,57 @@
 import React from 'react'
 import Form from '../containers/Form'
+import Button from "../components/Button";
+import Field from "../components/Field";
+import { BUTTON_TYPES } from "../helpers/buttonsTypes";
+import { FIELD_TYPES } from '../helpers/fieldTypes';
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+  const navigate = useNavigate();
+  const postURL = `${import.meta.env.VITE_DATABASE_URL}/user/login`;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const logs = {
+      email: e.target.username.value,
+      password: e.target.password.value,
+    };
+
+    try {
+      const response = await fetch(postURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(logs),
+      });
+      if(!response.ok) {
+        throw new Error("Identifiants incorrects");
+      }
+
+      const data = await response.json();
+      localStorage.setItem("token", data.body.token);
+      navigate("/user");
+    } catch (error) {
+      console.error("Erreur de connexion :", error.message);
+    };
+    
+  };
+  
   return (
     <main className='main bg-dark'>
       <section className="sign-in-content">
       <i className="fa fa-user-circle sign-in-icon"></i>
       <h2>Sign In</h2>
-      <Form />
+      <Form onSubmit={(e) => onSubmit(e)}>
+        <Field label="Username" id="username" name="username" className="input-wrapper" required/>
+        <Field type={FIELD_TYPES.INPUT_PASSWORD} label="Password" id="password" name="password" className="input-wrapper" required/>
+        <Field type={FIELD_TYPES.INPUT_CHECKBOX} label="Remember me" id="remember-me" name="remember-me" className="input-remember"/>
+        <Button type={BUTTON_TYPES.SUBMIT} className="sign-in-button" children="Sign In"/>
+      </Form>
       </section>
     </main>
-  )
-}
+  );
+};
 
 export default SignIn
