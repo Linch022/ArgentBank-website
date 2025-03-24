851746afc39e1ef81eb2b03ef7e75fc6e8d4fe9d
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-03-24 14:10:45 +0100

    Ajout de la fonctionnalité de logout dans le menu

--- my-react-app/src/containers/menu/Menu.jsx
+++ my-react-app/src/containers/menu/Menu.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Logo from '../../components/logo/Logo'
 import LogoImg from "../../assets/img/argentBankLogo.png"
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '../../redux/slices/userDataSlice'
 
 function Menu() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  console.log(user);
+  
+  const logOut = () => {
+    console.log("toto");
+    
+    dispatch(setUser(null));
+    localStorage.removeItem("token");
+  }
   
   const logoData = {
     src: LogoImg,
@@ -17,10 +29,23 @@ function Menu() {
         <h1 className="sr-only">Argent Bank</h1>
     </NavLink>
     <div>
-      <NavLink to="/sign-in" className="main-nav-item">
+      { user ? 
+      <>
+        <NavLink to="/user" className="main-nav-item">
+          <i className="fa fa-user-circle"></i>
+          {` ${user?.firstName}`}
+        </NavLink>
+        <NavLink to="/" className="main-nav-item" onClick={() => logOut()}>
+        <i className="fa fa-sign-out"></i>
+          {" Sign Out"}
+        </NavLink>
+      </>
+      :         
+      <NavLink to="/sign-in" className="main-nav-item" >
       <i className="fa fa-user-circle"></i>
       {" Sign In"}
       </NavLink>
+      }
     </div>
   </nav>
   );
