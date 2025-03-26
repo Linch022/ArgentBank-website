4f51f64ea38b4344ed9baedfcc96d4bad4600d5e
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-03-26 22:47:44 +0100

    Modification du composant hero afin de le simplifier en supprimant le composant heroContent

--- my-react-app/src/components/hero/Hero.jsx
+++ my-react-app/src/components/hero/Hero.jsx
@@ -1,15 +1,30 @@
-import { PropTypes } from 'prop-types';
+import { PropTypes } from "prop-types";
 
-function Hero({ backgroundImage, children }) {
-    return (
-      <div className="hero" style={{ backgroundImage: `url(${backgroundImage})` }}>
-        {children}
-      </div>
-    );
-  };
+function Hero({ content }) {
+	return (
+		<div className={`hero ${content.className}`}>
+			<section className='hero-content'>
+				{content.title && (
+					<h2 className={content.srOnlyTitle ? "sr-only" : ""}>
+						{content.title}
+					</h2>
+				)}
+				{content.subtitles.map((subtitle, index) => (
+					<p
+						key={index}
+						className='subtitle'
+					>
+						{subtitle}
+					</p>
+				))}
+				<p className='text'>{content.text}</p>
+			</section>
+		</div>
+	);
+}
 
 Hero.PropTypes = {
-    backgroundImage: PropTypes.string,
-    children: PropTypes.node,
+	backgroundImage: PropTypes.string,
+	children: PropTypes.node,
 };
-export default Hero
+export default Hero;
--- my-react-app/src/pages/home/Home.jsx
+++ my-react-app/src/pages/home/Home.jsx
@@ -1,15 +1,22 @@
 import Hero from '../../components/hero/Hero';
-import HeroContent from '../../components/heroContent/HeroContent';
-import heroImg from '../../assets/img/bank-tree.jpeg';
 import Features from '../../containers/features/Features';
 
 function Home() {
+const heroContent = {
+  className: "hero-home",
+  title: "Promoted Content",
+  srOnlyTitle: true,
+  subtitles: [
+    "No fees.", 
+    "No minimum deposit.", 
+    "High interest rates."
+  ],
+  text: "Open a savings account with Argent Bank today!",
 
+}
   return (
     <main>
-      <Hero backgroundImage={heroImg}>
-        <HeroContent title="Promoted Content" srOnlyTitle subtitles={["No fees.", "No minimum deposit.", "High interest rates."]} text="Open a savings account with Argent Bank today!"/>
-      </Hero>
+      <Hero content={heroContent} />
       <Features />
     </main>
   );
